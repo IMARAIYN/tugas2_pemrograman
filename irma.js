f337798fd0e8a499da15239c9353fa3e35ebcd3a
IRMA NURMALASARI <123700377+IMARAIYN@users.noreply.github.com>
2023-10-04 18:24:27 +0700

    Add files via upload

---
+++ irma.js
@@ -0,0 +1,10 @@
+const firstName = "Irma"
+const lastName = "Nurmalasari"
+const Age = 22
+const iMarrige = Age > 22
+
+const menikah = iMarrige 
+console.log(firstName)
+console.log(lastName)
+console.log("Umur : " + Age)
+console.log("Status menikah ? " + menikah)
